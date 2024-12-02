0b4ddb9cddd66030bc75edce680ce817bef18d44
Chagaev George <gerodann@gmail.com>
2024-12-02 18:41:16 +0300

    Новая крутая функция

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def better_func():
+    print('Я более крутая новая функция!')
+
+
 def main():
     print('Точно моя первая программа git в PyCharm!')
     print("Вношу изменения в репозиторий")  # TODO: Арбуз; by: chagaevea; date: 28.11.2024;
