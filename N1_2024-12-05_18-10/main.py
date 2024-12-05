bcab0204aeb5a07925d809d3406e87b8232a0a6f
Chagaev George <gerodann@gmail.com>
2024-12-05 18:52:20 +0300

    Добавил функции добавления текста и квадрата

--- N1_2024-12-05_18-10/main.py
+++ N1_2024-12-05_18-10/main.py
@@ -1,29 +1,45 @@
 import pygame
 
 
-def main() -> None:
-    # pygame setup
+def draw1(screen: pygame.display) -> None:
+    screen.fill("#121212")
+    font = pygame.font.Font(None, 50)
+    text = font.render("Hello, PyGame!", True, '#FFFFFF')
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, '#353535',
+                     (text_x - 10, text_y - 10, text_w + 20, text_h + 20), 1)
+
+
+def draw_square(screen: pygame.display) -> None:
+    color = pygame.Color('#303030')
+    pygame.draw.rect(screen, color, (20, 20, 100, 100), 0)
+
+    hsv = color.hsva
+
+    color.hsva = (hsv[0], hsv[1], hsv[2] + 30, hsv[3] - 0.5)
+    pygame.draw.rect(screen, color, (10, 10, 100, 100), 0)
+
+
+if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    size = width, height = (800, 600)
+    my_screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
     running = True
 
     while running:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # fill the screen with a color to wipe away anything from last frame
-        screen.fill("#121212")
+        draw1(my_screen)
+        draw_square(my_screen)
 
-        # RENDER YOUR GAME HERE
-
-        # flip() the display to put your work on screen
         pygame.display.flip()
+        clock.tick(30)
 
     pygame.quit()
-
-
-if __name__ == '__main__':
-    main()  # Example file showing a basic pygame "game loop"
