b86e512d0054eb3609eabf57308dd92d90b2e312
Chagaev George <gerodann@gmail.com>
2024-12-12 18:36:06 +0300

    Построение поля + его настройка

--- N3_2024-12-12_18-16/main.py
+++ N3_2024-12-12_18-16/main.py
@@ -2,23 +2,34 @@ import pygame
 
 
 class Board:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.w = width
         self.h = height
         self.board = [[None] * width for _ in range(height)]
         self.left = 10
-        self.right = 10
+        self.top = 10
         self.cell_size = 30
 
-    def render(self, screen: pygame.surface.Surface):
-        pass
+    def render(self, screen: pygame.surface.Surface) -> None:
+        for x in range(self.w):
+            for y in range(self.h):
+                pygame.draw.rect(screen, pygame.Color('#CCCCCC'), (x * self.cell_size + self.left,
+                                                                   y * self.cell_size + self.top,
+                                                                   self.cell_size, self.cell_size), 1)
+
+    def set_view(self, left: int, top: int, cell_size: int) -> None:
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
 
 
-def main():
+def main() -> None:
+    """main function of the project"""
     pygame.init()
     size = width, height = 1280, 720
     screen = pygame.display.set_mode(size)
-    board = Board(7, 5)
+    board = Board(18, 12)
+    board.set_view(100, 100, 15)
     clock = pygame.time.Clock()
     running = True
     v = 100
