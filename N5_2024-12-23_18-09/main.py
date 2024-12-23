d8aa04fbabb62219eebe7f014c6a81eb6598e137
Chagaev George <gerodann@gmail.com>
2024-12-23 18:39:45 +0300

    Пересечение прямоугольников

--- N5_2024-12-23_18-09/main.py
+++ N5_2024-12-23_18-09/main.py
@@ -1,8 +1,14 @@
+def func(x11: int, x12: int, x21: int, x22: int) -> bool:
+    return x21 <= x11 <= x22 or x11 <= x21 <= x12
+
+
 def main() -> int:
-    x1, y1, r1 = map(int, input().split())
-    x2, y2, r2 = map(int, input().split())
-    d = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-    print('YES' if d <= r1 + r2 else 'NO')
+    x1, y1, w1, h1 = map(int, input().split())
+    x2, y2, w2, h2 = map(int, input().split())
+    if func(x1, x1 + w1, x2, x2 + w2) and func(y1, y1 + h1, y2, y2 + h2):
+        print("YES")
+    else:
+        print("NO")
     return 0
 
 
